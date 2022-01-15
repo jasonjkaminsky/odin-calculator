a3b3d6fad6ea8738d272237e28753a3e8644de61
Jason Kaminsky <jason.j.kaminsky@gmail.com>
2022-01-15 16:01:55 -0500

    First script.js

---
+++ script.js
@@ -0,0 +1,70 @@
+const displayValue = document.querySelector(".displayValue");
+const acBtn = document.querySelector(".AC");
+const zeroBtn = document.querySelector(".zero");
+const oneBtn = document.querySelector(".one");
+const twoBtn = document.querySelector(".two");
+const threeBtn = document.querySelector(".three");
+const fourBtn = document.querySelector(".four");
+const fiveBtn = document.querySelector(".five");
+const sixBtn = document.querySelector(".six");
+const sevenBtn = document.querySelector(".seven");
+const eightBtn = document.querySelector(".eight");
+const nineBtn = document.querySelector(".nine");
+const dotBtn = document.querySelector(".dot");
+const equalsBtn = document.querySelector(".equals");
+
+const addBtn = document.querySelector("#add");
+const subtractBtn = document.querySelector("#subtract");
+const multiplyBtn = document.querySelector("#multiply");
+const divideBtn = document.querySelector("#divide");
+
+displayValue.textContent = "0123456789x=+ ";
+
+// displayValue =
+
+// ODIN: Create a new function "operate" that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+// let num1 =
+// let operator =
+// let num2 =
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// ODIN: Create the functions that populate the display when you click the number buttons… you should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+///// event listeners /////
+// acBtn.addEventListener("click");
+// zeroBtn.addEventListener("click");
+// oneBtn.addEventListener("click");
+// twoBtn.addEventListener("click", );
+// threeBtn.addEventListener("click");
+// fourBtn.addEventListener("click");
+// fiveBtn.addEventListener("click");
+// sixBtn.addEventListener("click");
+// sevenBtn.addEventListener("click");
+// eightBtn.addEventListener("click");
+// nineBtn.addEventListener("click");
+// dotBtn.addEventListener("click");
+// addBtn.addEventListener("click");
+// subtractBtn.addEventListener("click");
+// multiplyBtn.addEventListener("click");
+// divideBtn.addEventListener("click");
+// equalsBtn.addEventListener("click");
